7f47c84254b0a8a2cc02564be20f28ed697440dd
Scott McDermid <scott@blockchain.ventures>
2025-06-24 13:11:42 -0700

    Fixed absorb skill to present skill dropdown Fixed select-range bug for restricted ranges Added red text if insufficient skill level

--- src/components/ActiveSpellSummary.tsx
+++ src/components/ActiveSpellSummary.tsx
@@ -12,6 +12,7 @@ import {
 import { Divider, Tooltip } from '@mui/material';
 import FlashOn from '@mui/icons-material/FlashOn';
 import AttachMoney from '@mui/icons-material/AttachMoney';
+import { cn } from '@/utils/cn';
 
 export default function ActiveSpellEffects() {
   const { addedEffects, skills, luck } = useSpellStore();
@@ -62,12 +63,24 @@ export default function ActiveSpellEffects() {
     <div className="w-full max-w-md shadow-sm">
       <Divider />
       {school && mastery && (
-        <div className="mt-4 flex items-center justify-end gap-4 text-lg">
+        <div
+          className={cn(
+            'mt-4 flex items-center justify-end gap-4 text-lg',
+            minLevel > skills[school] && 'text-error',
+          )}
+        >
           {school} {mastery}
         </div>
       )}
-      {minLevel > 0 && (
-        <div className="flex items-center justify-end gap-4 text-sm">Level {minLevel}</div>
+      {school && minLevel > 0 && (
+        <div
+          className={cn(
+            'flex items-center justify-end gap-4 text-sm',
+            minLevel > skills[school] && 'text-error',
+          )}
+        >
+          Level {minLevel}
+        </div>
       )}
       <div className="mt-4 flex justify-end gap-4 text-lg">
         <div className="mt-4 flex items-center gap-4 text-lg">
